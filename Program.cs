beb9c713a2c23029ba49587e17bcb9211ab59868
Andre Henrique <andrehenriqueleme.lopes@gmail.com>
2024-10-06 23:42:48 -0300

    Demo1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+class AulaRef
+{
+    static void Demo1()
+    {
+         int a = 5;
+        Adicionar20(ref a);
+        WriteLine($"O valor sera {a}!");
+    }
+    static void Adicionar20(ref int a)
+    {
+        a += 20;
+    }
+
+    static void AlterarNome(string[] nomes,string nome, string nomeNovo)
+    {
+        for (int i = 0; i < nomes.Length; i++)
+        {
+            if(nomes[i] == nome)
+            {
+                nomes[i] = nomeNovo;
+            }
+        }
+    }
+    static void Main()
+    {
+        var nomes = new string []{"Jose","Maria","Ricardo","Lucas"};
+        
+         WriteLine($@"A lista de nomes alterados é:
+         {string.Join(", \n",nomes)}");
+        
+        WriteLine("Digite o nome a se mudado!:");
+        var nome = ReadLine();
+        WriteLine("Digite um nome novo!:");
+        var nomeNovo = ReadLine();
+
+        AlterarNome(nomes,nome,nomeNovo);
+
+        WriteLine($@"A lista de nomes alterados é:
+        {string.Join(", \n",nomes)}");
+    }
+}
